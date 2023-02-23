512e4da6f652ac4c917358c880fb514d6e793a43
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-23 23:40:02 +0100

    Add files via upload

---
+++ Zadanie 1.2/Zadanie 1.2/Zadanie 1.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+
+    if (a % 2 == 0)
+    {
+        cout << "liczba jest parzysta" << endl;
+    }
+    else
+    {
+        cout << "liczba jest nie parzysta" << endl;
+    }
+}
+
