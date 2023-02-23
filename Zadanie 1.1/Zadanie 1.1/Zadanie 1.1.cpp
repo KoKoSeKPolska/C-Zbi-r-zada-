036bfb6116df81d5647b1b4a41091885348565c5
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-23 23:39:41 +0100

    Add files via upload

---
+++ Zadanie 1.1/Zadanie 1.1/Zadanie 1.1.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    unsigned a;
+    cout << "Podaj 3 liczby nie ujemne : " << endl;
+    vector <unsigned> tab;
+    for (int i = 0; i <= 2; i++)
+    {
+        cin >> a;
+        tab.push_back(a);
+    }
+    unsigned x = 0;
+    int suma;
+    for (int i = 0; i < tab.size(); i++)
+    {
+        suma =+ tab[i];
+        if (tab[i] > x)
+        {
+            x = tab[i];
+        }
+
+    }
+    for (int i = 1; i <= x; i++)
+    {
+        cout << suma << endl;
+    }
+
+}
