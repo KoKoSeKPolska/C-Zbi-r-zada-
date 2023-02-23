08cdb7cf86db352906a9f458fd647bc1cfa40d28
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-24 00:08:05 +0100

    Add files via upload

---
+++ Zadanie 1.7/Zadanie 1.7/Zadanie 1.7.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ile = 0;
+    char x;
+    while (cin >> x)
+    {
+        ile++;
+        if (x == 'x')
+        {
+            cout << "liczba prob: " << ile << endl;
+            return 0;
+        }
+    }
+}
