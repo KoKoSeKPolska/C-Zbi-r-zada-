0e7e850288a09afdba796acaecd9286de6c86f14
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-23 23:41:09 +0100

    Add files via upload

---
+++ Zadanie 1.5/Zadanie 1.5/Zadanie 1.5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float a1, a2, a3, a4, a5;
+    cout << "Wzór ((( a1 + a2) * a3) - a4) / a5:" << endl;
+    cin >> a1 >> a2 >> a3 >> a4 >> a5;
+    if (a5 == 0.0)
+    {
+        cout << "Nie wykonam działania, nie dziele przez 0" << endl;
+    }
+    else
+    {
+        float wynik = ((((a1 + a2) * a3) - a4) / a5);
+        cout << "(((" << a1 << " + " << a2 << ") * " << a3 << ") - " << a4 << ") / " << a5 << " = " << wynik << endl;
+    }
+
+
+}
