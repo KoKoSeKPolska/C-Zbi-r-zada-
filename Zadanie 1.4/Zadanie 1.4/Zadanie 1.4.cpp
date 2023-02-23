847f6d39f841a811e1d77a6a536d0d19774eedef
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-23 23:40:48 +0100

    Add files via upload

---
+++ Zadanie 1.4/Zadanie 1.4/Zadanie 1.4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<char> samogloski = { 'a','e','i','o','u','y' };
+	char a;
+	cout << "sprawdz czy to samogłoska" << endl;
+	cin >> a;
+	if (a >= 48 && a <= 48 + 9)
+	{
+		cout << "to liczba" << endl;
+		return 0;
+	}
+	for (auto s : samogloski)
+	{
+		if (a == s)
+		{
+			cout << " " << a << " jest samogłoska" << endl;
+		}
+		else
+		{
+			cout << a << "to spolgloska " << endl;
+		}
+		break;
+	}
+
+}
