95615621e5c88767132e4ec7f94aaaf26eafb181
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-24 00:01:55 +0100

    Add files via upload

---
+++ Zadanie 1.6/Zadanie 1.6/Zadanie 1.6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int ile_p = 0, ile_np = 0;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cin >> a;
+        if (a % 2 == 1)
+        {
+            ile_np++;
+        }
+        else
+        {
+            ile_p++;
+        }
+    }
+    cout << "Ilosc liczb parzystych:" << ile_p << endl;
+    cout << "Ilosc liczb nieparzystych:" << ile_np << endl;
+}
+
