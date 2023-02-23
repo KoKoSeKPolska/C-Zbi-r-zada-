aad030d93f1a7a992406633815de3ef382838a5b
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-23 23:40:28 +0100

    Add files via upload

---
+++ Zadanie 1.3/Zadanie 1.3/Zadanie 1.3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    if (a % 3 == 0 && a % 5 == 0)
+    {
+        cout << a << " jest podzielne przez 3 i 5" << endl;
+    }
+    else if (a % 3== 0 && a % 5 != 0)
+    {
+        cout << a << " jest podzielne przez 3 ale nie przez 5" << endl;
+    }
+    else if (a % 3 != 0 && a % 5 == 0)
+    {
+        cout << a << " jest podzielne przez 5 ale nie przez 3" << endl;
+    }
+    else if (!(a % 3 == 0 && a % 5 == 0))
+    {
+        cout << a << " nie jest podzielne przez 5 i 3" << endl;
+    }
+}
