5d26af4c699efe98a30123d9c2bba6557733a1ff
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-24 23:45:32 +0100

    Add files via upload

---
+++ Zadanie 1.16/Zadanie 1.16/Zadanie 1.16.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int x = 100, y = 0;
+    for(int i = 1; i <= 100; i++)
+    {
+        x = x - y++;
+        cout << x << " ";
+    }
+}
