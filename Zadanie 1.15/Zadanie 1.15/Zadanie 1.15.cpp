4c21edc5d072620412b4c60785ee25d532fc870d
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-24 23:30:48 +0100

    Add files via upload

---
+++ Zadanie 1.15/Zadanie 1.15/Zadanie 1.15.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 1, y = 0;
+    int ile = 1;
+    while (ile <= 100)
+    {
+        if (y++ < x)
+        {
+            cout << x << " ";
+            ile++;
+        }
+        else
+        {
+            y = 0;
+            x++;
+        }
+    }
+    
+
+}
