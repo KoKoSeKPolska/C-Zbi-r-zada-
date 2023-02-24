acd353287b594b8f9246b7c4831c1606ee2b07b9
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-24 03:00:01 +0100

    Add files via upload

---
+++ Zadanie 1.9/Zadanie 1.9/Zadanie 1.9.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+bool parz(int x)
+{
+    if (x % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+{
+    int x;
+    cout << "podaj liczbe calkowita: " << endl;
+    cin >> x;
+    if (x < 0)
+    {
+        x--;
+        cout << x << endl;
+        if (parz(x) == true)
+        {
+            cout << "liczba " << x << " jest parzysta" << endl;
+        }
+        else
+        {
+            cout << "liczba " << x << " jest nieparzysta" << endl;
+        }
+    }
+    else if (x > 0)
+    {
+        x++;
+        cout << x << endl;
+        if (parz(x) == true)
+        {
+            cout << "liczba " << x << " jest parzysta" << endl;
+        }
+        else
+        {
+            cout << "liczba " << x << " jest nieparzysta" << endl;
+        }
+    }
+    else if (x == 0)
+    {
+        cout << x << endl;
+    }
+
+    return 0;
+}
