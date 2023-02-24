1f3471b44353f1b5c89281b5a89152d0a6523a63
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-24 04:46:35 +0100

    Add files via upload

---
+++ Zadanie 1.13/Zadanie 1.13/Zadanie 1.13.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 0, y = 0;
+    for (int i = 1; i <= 120; i++)
+    {
+        if (i % 5 == 0 && i % 11 == 0)
+        {
+            y++;
+     
+        }
+        else
+        {
+            x++;
+            cout << i << " ";
+        }
+    }
+    cout << "liczb pominiętych: " << y << endl;
+    cout << "ilość pozostałych liczb: " << x << endl;
+}
