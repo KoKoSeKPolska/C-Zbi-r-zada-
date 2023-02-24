59c221f815210d98fcb47a9d9e6a0db6c65cd7f1
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-24 14:41:22 +0100

    Add files via upload

---
+++ Zadanie 1.14/Zadanie 1.14/Zadanie 1.14.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int>tab = { 3,1,2,1};
+    size_t x= 1;
+    while (x <= 100)
+    {
+        cout << tab[(x++  - 1) % 4] << " ";
+    }
+}
