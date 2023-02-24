3cf2465a6f3d1259ecb997aef254a435eaa2a77b
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-25 00:02:38 +0100

    Add files via upload

---
+++ Zadanie 1.17/Zadanie 1.17/Zadanie 1.17.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 6, y = 2;
+    int i = 1;
+    while (i <= 50)
+    {
+        cout << x << " " << y << " ";
+        x += 2;
+        y += 1;
+        i++;
+    }
+}
+
