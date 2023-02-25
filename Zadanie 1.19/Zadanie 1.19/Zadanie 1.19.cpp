651c91671d30cabce3d10e398868e68b3a7b08f6
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-25 03:02:43 +0100

    Add files via upload

---
+++ Zadanie 1.19/Zadanie 1.19/Zadanie 1.19.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 6 == 0)
+        {
+            cout << i << " ";
+        }
+    }
+}
