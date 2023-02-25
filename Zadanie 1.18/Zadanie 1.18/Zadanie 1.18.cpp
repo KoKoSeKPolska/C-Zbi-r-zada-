4c5722400b8943a298798248b67b5fb5b8e8bd9b
KoKoSeKPolska <116282831+KoKoSeKPolska@users.noreply.github.com>
2023-02-25 02:59:29 +0100

    Add files via upload

---
+++ Zadanie 1.18/Zadanie 1.18/Zadanie 1.18.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 0 , y = 0;
+    while (y == 0)
+    {
+        cin >> x;
+        if (x <= 10 and x > 1)
+        {
+            cout << "twoja liczba: " << x << endl;
+            y++;
+        }
+        else
+        {
+            cout << x * 2 << endl;
+        }
+    }
+}
